f3ae5f479c9854cec6e74ab4bb2bc53e50261202
SilvioAlvarez67 <104239950+SilvioAlvarez67@users.noreply.github.com>
2022-04-26 22:46:05 -0300

    Initial commit

---
+++ src/avanzaryquitar.ts
@@ -0,0 +1,23 @@
+console.log ("ejercicio1");
+console.log ("avanzar y quitar");
+console.log ("avanzar y quitar");
+console.log ("avanzar y quitar");
+console.log ("avanzar y quitar");
+console.log ("girar a la izquierda");
+console.log ("avanzar y quitar");
+console.log ("avanzar y quitar");
+console.log ("avanzar y quitar");
+console.log ("avanzar y quitar");
+console.log ("avanzar y quitar");
+
+console.log ("ejercicio2");
+console.log ("avanzar y llenar");
+console.log ("avanzar");
+console.log ("avanzar");
+console.log ("avanzar y llenar");
+console.log ("avanzar y quitar");
+console.log ("avanzar y quitar");
+
+console.log ("ejercicio3");
+console.log ("avanzar y quitar 5");
+console.log ("avanzar y quitar");
